abf2f6f6442c0ebf662139bb6dc425a8128d44f4
annisafzahra <annisafathimatuzzahra@gmail.com>
2026-07-21 11:14:54 +0700

    update soal

--- lib/data/dataSoal.tsx
+++ lib/data/dataSoal.tsx
@@ -2,44 +2,44 @@ import { SoalType } from "@/type/soalType";
 
 export const soalList: SoalType[] = [
     // REALISTIC (R)
-    { id: 1, pertanyaan: "Saya senang memperbaiki barang elektronik atau mesin", dimensi: "R" },
-    { id: 2, pertanyaan: "Saya tertarik dengan pekerjaan lapangan atau teknis", dimensi: "R" },
-    { id: 3, pertanyaan: "Saya suka menggunakan alat-alat seperti obeng, tang, dll", dimensi: "R" },
-    { id: 4, pertanyaan: "Saya lebih suka praktik langsung daripada teori", dimensi: "R" },
-    { id: 5, pertanyaan: "Saya tertarik pada dunia otomotif atau mesin", dimensi: "R" },
+    { id: 1, pertanyaan: "Saya senang merakit atau membongkar suatu benda untuk melihat cara kerjanya", dimensi: "R" },
+    { id: 2, pertanyaan: "Saya lebih menikmati kegiatan praktik daripada hanya membaca teori", dimensi: "R" },
+    { id: 3, pertanyaan: "Saya tertarik menggunakan peralatan atau mesin dalam suatu pekerjaan", dimensi: "R" },
+    { id: 4, pertanyaan: "Saya suka memperbaiki benda yang rusak", dimensi: "R" },
+    { id: 5, pertanyaan: "Saya merasa nyaman bekerja di luar ruangan atau lingkungan kerja yang aktif", dimensi: "R" },
     
     // INVESTIGATIVE (I)
     { id: 6, pertanyaan: "Saya suka memecahkan masalah yang rumit", dimensi: "I" },
-    { id: 7, pertanyaan: "Saya tertarik dengan eksperimen atau penelitian", dimensi: "I" },
-    { id: 8, pertanyaan: "Saya senang menganalisis data atau informasi", dimensi: "I" },
-    { id: 9, pertanyaan: "Saya suka pelajaran matematika atau sains", dimensi: "I" },
-    { id: 10, pertanyaan: "Saya tertarik mencari tahu bagaimana sesuatu bekerja", dimensi: "I" },
+    { id: 7, pertanyaan: "Saya tertarik dengan eksperimen atau pengamatan", dimensi: "I" },
+    { id: 8, pertanyaan: "Saya senang mencari informasi sebelum mengambil kesimpulan", dimensi: "I" },
+    { id: 9, pertanyaan: "Saya menikmati pelajaran yang membutuhkan analisis dan pemikiran mendalam", dimensi: "I" },
+    { id: 10, pertanyaan: "Saya tertarik mempelajari bagaimana suatu hal dapat terjadi", dimensi: "I" },
   
     // // ARTISTIC (A)
-    { id: 11, pertanyaan: "Saya suka menggambar atau membuat desain", dimensi: "A" },
-    { id: 12, pertanyaan: "Saya senang mengekspresikan diri melalui seni", dimensi: "A" },
-    { id: 13, pertanyaan: "Saya tertarik pada musik, lukisan, atau karya kreatif", dimensi: "A" },
-    { id: 14, pertanyaan: "Saya suka membuat sesuatu yang unik dan kreatif", dimensi: "A" },
-    { id: 15, pertanyaan: "Saya lebih suka kebebasan daripada aturan yang kaku", dimensi: "A" },
+    { id: 11, pertanyaan: "Saya senang menciptakan ide-ide baru", dimensi: "A" },
+    { id: 12, pertanyaan: "Saya lebih suka pekerjaan yang memberi kebebasan berkreasi", dimensi: "A" },
+    { id: 13, pertanyaan: "Saya suka mengekspresikan diri melalui tulisan, gambar, atau karya kreatif lainnya", dimensi: "A" },
+    { id: 14, pertanyaan: "Saya suka membuat sesuatu yang unik dan berbeda", dimensi: "A" },
+    { id: 15, pertanyaan: "Saya lebih suka kegiatan yang melibatkan imajinasi daripada aturan yang kaku", dimensi: "A" },
   
     // // SOCIAL (S)
-    { id: 16, pertanyaan: "Saya senang membantu orang lain", dimensi: "S" },
-    { id: 17, pertanyaan: "Saya suka bekerja dalam tim", dimensi: "S" },
-    { id: 18, pertanyaan: "Saya nyaman berkomunikasi dengan banyak orang", dimensi: "S" },
-    { id: 19, pertanyaan: "Saya tertarik menjadi guru, konselor, atau pembimbing", dimensi: "S" },
-    { id: 20, pertanyaan: "Saya merasa puas jika bisa membantu orang lain", dimensi: "S" },
+    { id: 16, pertanyaan: "Saya senang membantu teman yang mengalami kesulitan", dimensi: "S" },
+    { id: 17, pertanyaan: "Saya merasa senang bekerja sama dengan orang lain", dimensi: "S" },
+    { id: 18, pertanyaan: "Saya senang menjelaskan sesuatu kepada orang lain", dimensi: "S" },
+    { id: 19, pertanyaan: "Saya tertarik pada kegiatan yang melibatkan banyak interaksi dengan orang lain", dimensi: "S" },
+    { id: 20, pertanyaan: "Saya merasa puas ketika dapat membantu seseorang memahami sesuatu", dimensi: "S" },
   
     // // ENTERPRISING (E)
-    { id: 21, pertanyaan: "Saya suka memimpin atau mengatur orang lain", dimensi: "E" },
-    { id: 22, pertanyaan: "Saya tertarik pada dunia bisnis atau usaha", dimensi: "E" },
-    { id: 23, pertanyaan: "Saya suka berbicara untuk meyakinkan orang lain", dimensi: "E" },
-    { id: 24, pertanyaan: "Saya ingin memiliki usaha sendiri", dimensi: "E" },
+    { id: 21, pertanyaan: "Saya senang mengajak orang lain mengikuti ide saya", dimensi: "E" },
+    { id: 22, pertanyaan: "Saya merasa percaya diri berbicara di depan banyak orang", dimensi: "E" },
+    { id: 23, pertanyaan: "Saya suka mengambil inisiatif dalam suatu kegiatan", dimensi: "E" },
+    { id: 24, pertanyaan: "Saya tertarik mengatur atau mengoordinasikan suatu kelompok", dimensi: "E" },
     { id: 25, pertanyaan: "Saya senang mengambil keputusan penting", dimensi: "E" },
   
     // // CONVENTIONAL (C)
-    { id: 26, pertanyaan: "Saya suka pekerjaan yang rapi dan terstruktur", dimensi: "C" },
+    { id: 26, pertanyaan: "Saya senang menyusun sesuatu agar lebih rapi dan teratur", dimensi: "C" },
     { id: 27, pertanyaan: "Saya teliti dalam mengerjakan sesuatu", dimensi: "C" },
-    { id: 28, pertanyaan: "Saya senang mengelola data atau angka", dimensi: "C" },
-    { id: 29, pertanyaan: "Saya nyaman bekerja dengan aturan yang jelas", dimensi: "C" },
-    { id: 30, pertanyaan: "Saya tertarik pada administrasi atau akuntansi", dimensi: "C" },
+    { id: 28, pertanyaan: "Saya nyaman mengikuti aturan yang jelas", dimensi: "C" },
+    { id: 29, pertanyaan: "Saya suka mengelola data atau informasi secara sistematis", dimensi: "C" },
+    { id: 30, pertanyaan: "Saya memperhatikan detail-detail kecil yang sering terlewat oleh orang lain", dimensi: "C" },
   ];
